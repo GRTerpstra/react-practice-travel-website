f9a90384ebfaedd95c4fcba3877d98a38a92414e
Gerwin <gerwin_terpstra@live.nl>
2022-01-03 23:53:59 +0100

    eerste commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Pages/Home';
+import Products from './Components/Pages/Products';
+import Services from './Components/Pages/Services';
+import SignUp from './Components/Pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+          <Router>
+            <Navbar />
+              <Routes>
+                  <Route path='/' exact element={<Home />} />
+                  <Route path='/products' exact element={<Products />} />
+                  <Route path='/services' exact element={<Services />} />
+                  <Route path='/sign-up' exact element={<SignUp />} />
+              </Routes>
+          </Router>
     </div>
   );
 }
